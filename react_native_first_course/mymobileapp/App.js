36f9a9cd18254b55d15ccb72a95daccb7b5c3d06
alexg622 <alex.gm62288@gmail.com>
2018-08-24 18:52:56 -0700

    Flex component

--- react_native_first_course/mymobileapp/App.js
+++ react_native_first_course/mymobileapp/App.js
@@ -1,21 +1,28 @@
 import React, {Component} from 'react';
-import {AppRegistry, Text, View} from 'react-native';
+import {StyleSheet, AppRegistry, Text, View} from 'react-native';
 import SimpleComponent1 from './app/components/SimpleComponent1/SimpleComponent1'
 import TextInputComponent from './app/components/TextInputComponent/TextInputComponent'
 import SliderComponent from './app/components/SliderComponent/SliderComponent'
 import PickerComponent from './app/components/PickerComponent/PickerComponent'
 import SwitchComponent from './app/components/SwitchComponent/SwitchComponent'
 import StyledComponent from './app/components/StyledComponent/StyledComponent'
+import FlexComponent from './app/components/FlexComponent/FlexComponent'
 
 
 export default class mymobileapp extends Component{
   render(){
     return(
-      <View>
-        <StyledComponent />
+      <View style={styles.container}>
+        <FlexComponent />
       </View>
     )
   }
 }
 
+const styles = StyleSheet.create({
+  // container: {
+  //   justifyContent: "center",
+  //   alignItems: "center"
+  // }
+})
 AppRegistry.registerComponent('mymobileapp', () => mymobileapp)
