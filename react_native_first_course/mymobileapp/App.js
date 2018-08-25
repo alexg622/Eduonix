878d7c163f83b2bd7a73a88195f57bec9e3110e1
alexg622 <alex.gm62288@gmail.com>
2018-08-24 18:27:53 -0700

    Make style component

--- react_native_first_course/mymobileapp/App.js
+++ react_native_first_course/mymobileapp/App.js
@@ -5,13 +5,14 @@ import TextInputComponent from './app/components/TextInputComponent/TextInputCom
 import SliderComponent from './app/components/SliderComponent/SliderComponent'
 import PickerComponent from './app/components/PickerComponent/PickerComponent'
 import SwitchComponent from './app/components/SwitchComponent/SwitchComponent'
+import StyledComponent from './app/components/StyledComponent/StyledComponent'
 
 
 export default class mymobileapp extends Component{
   render(){
     return(
       <View>
-        <SwitchComponent />
+        <StyledComponent />
       </View>
     )
   }
---
+++ react_native_first_course/mymobileapp/app/components/StyledComponent/StyledComponent.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import {StyleSheet, AppRegistry, Text, View} from 'react-native';
+
+export default class StyledComponent extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render(){
+    return(
+      <View style={styles.container}>
+        <Text style={styles.myText}>Styled Component</Text>
+        <View style={styles.view1}></View>
+        <View style={styles.view2}></View>
+        <View style={styles.view3}></View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+    paddingLeft: 50,
+  },
+  myText: {
+    color: "blue",
+    fontSize:20,
+    textAlign: "center",
+    fontWeight: "bold",
+    textShadowColor: "black",
+    textShadowOffset: {width:1, height:1},
+    borderBottomWidth: 2,
+    borderBottomColor: "black",
+    padding: 5
+  },
+  view1: {
+    width: 100,
+    height: 50,
+    backgroundColor: "#FF5300"
+  },
+  view2: {
+    width: 200,
+    height: 100,
+    backgroundColor: "#FF7431"
+  },
+  view3: {
+    width: 300,
+    height: 150,
+    backgroundColor: "#FF9700"
+  }
+})
+
+AppRegistry.registerComponent('StyledComponent', () => StyledComponent)
